31a3a2a97d32e859773b802f1cbd903ca1ba6f16
Jaypatel123 <jaypatel97043@gmail.com>
2023-06-30 06:52:28 -0400

    admin dashboard changed

--- projects/admin.py
+++ projects/admin.py
@@ -3,6 +3,7 @@ from .models import Project, Review, Tag
 
 class ProjectAdmin(admin.ModelAdmin):
     list_display= ['title', 'owner']
+    search_fields = ['title']
 # Register your models here.
 admin.site.register(Project, ProjectAdmin)
 admin.site.register(Review)
